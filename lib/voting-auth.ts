ba3fc4faf4bef68321e8387b95e0fc8f836c5c7d
szymonso1973-creator <szymonso1973@gmail.com>
2026-03-18 22:07:19 +0100

    Delete lib directory

--- lib/voting-auth.ts
+++
@@ -1,7 +0,0 @@
-import crypto from "crypto";
-
-export function hashVotingToken(token: string) {
-  const secret = process.env.VOTING_TOKEN_SECRET;
-  if (!secret) throw new Error("MISSING_VOTING_TOKEN_SECRET");
-  return crypto.createHmac("sha256", secret).update(token).digest("hex");
-}
